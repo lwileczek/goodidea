d434fc2241f25a14b90d4fd6b513b41334e5adb6
Lk <pleazenospam@protonmail.com>
2023-10-26 08:49:23 -0400

    create new task

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Could not get all tasks from db", err)
 	}
 
-	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/footer.html"))
+	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/task.html", "templates/footer.html", "templates/create.html"))
 
 	err = tmpl.Execute(w, tsks)
 	if err != nil {
@@ -44,10 +44,37 @@ func updateScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, fmt.Sprintf("%d", score))
 }
 
+func createTask(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("Error parsing form", err)
+	}
+
+	title := r.FormValue("title")
+	body := r.FormValue("details")
+	err = addTask(title, body)
+	if err != nil {
+		fmt.Println("Could not update task score", err)
+	}
+
+	task := Task{
+		Title: title,
+		Body:  &body,
+		Score: 0,
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/make-task.html", "templates/task.html"))
+	err = tmpl.Execute(w, task)
+	if err != nil {
+		fmt.Println("Could not execute template", err)
+	}
+}
+
 func NewServer() *mux.Router {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/tasks/{id}/score", updateScore).Methods("POST")
+	mux.HandleFunc("/tasks", createTask).Methods("POST")
 
 	return mux
 }
